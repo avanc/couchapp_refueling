7e2376d77a275e8ee2dad568d9168c1f3170dca8
Sven Klomp <mail@klomp.eu>
2013-07-28 16:19:33 +0200

    Added AJAX interaction

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,66 +1,134 @@
-// Apache 2.0 J Chris Anderson 2011
-$(function() {   
-    // friendly helper http://tinyurl.com/6aow6yn
-    $.fn.serializeObject = function() {
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function() {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
+$(document).ready(function(){
+    $("#tabs" ).tabs();
+    $("#datepicker" ).datepicker({ dateFormat: "yy-mm-dd", autoSize: true });
+    
+    resetForm();
+    
+    $("#refuel_form").submit(function() {
+        
+        formdata={};
+        formdata["type"]="refuel";
+        formdata["date"]=$("#datepicker").val();
+        formdata["odometer"]=$("#odometer").val();
+        formdata["amount"]=$("#amount").val();
+        formdata["price"]=$("#price").val();
+                       
+                      
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5984/car/",
+            data: JSON.stringify(formdata),
+            success: dataTransmitted,
+            dataType: "json", 
+            contentType:"application/json; charset=utf-8"
         });
-        return o;
-    };
 
-    var path = unescape(document.location.pathname).split('/'),
-        design = path[3],
-        db = $.couch.db(path[1]);
-    function drawItems() {
-        db.view(design + "/recent-items", {
-            descending : "true",
-            limit : 50,
-            update_seq : true,
-            success : function(data) {
-                setupChanges(data.update_seq);
-                var them = $.mustache($("#recent-messages").html(), {
-                    items : data.rows.map(function(r) {return r.value;})
-                });
-                $("#content").html(them);
-            }
-        });
-    };
-    drawItems();
-    var changesRunning = false;
-    function setupChanges(since) {
-        if (!changesRunning) {
-            var changeHandler = db.changes(since);
-            changesRunning = true;
-            changeHandler.onChange(drawItems);
-        }
-    }
-    $.couchProfile.templates.profileReady = $("#new-message").html();
-    $("#account").couchLogin({
-        loggedIn : function(r) {
-            $("#profile").couchProfile(r, {
-                profileReady : function(profile) {
-                    $("#create-message").submit(function(e){
-                        e.preventDefault();
-                        var form = this, doc = $(form).serializeObject();
-                        doc.created_at = new Date();
-                        doc.profile = profile;
-                        db.saveDoc(doc, {success : function() {form.reset();}});
-                        return false;
-                    }).find("input").focus();
-                }
-            });
-        },
-        loggedOut : function() {
-            $("#profile").html('<p>Please log in to see your profile.</p>');
-        }
+        return false; //Prevent form action
     });
- });
+
+
+    
+});
+
+
+var dataTransmitted = function (data, status) {
+    if (status == "success")
+    {
+        if (data["ok") {
+            resetForm();
+        }
+        else {
+            alert(data);
+        }
+            
+    }
+    else
+    {
+        alert(status);
+    }  
+
+    
+}
+
+
+function getHistory() {
+    $.get(
+        "diary.py",
+        { "action" : "getHistory"},
+        function(data, status) {
+            if (status == "success")
+            {
+                var counter=0;
+                while (data["entries"] && counter<10) {
+                    addHistoryItem(data["entries"].shift(), true);
+                    counter++;
+                }
+            }
+            else
+            {
+                showError(status);
+            }
+        },
+        "json"
+    );
+}
+
+
+var addData = function (text, reverse) {
+    var listitem;
+    
+    if (reverse)
+    {
+        $('#history').append(
+            listitem = $('<li>').append(text)
+        
+        );
+    }
+    else
+    {
+        $('#history').prepend(
+            listitem = $('<li>').append(text)
+        
+        );
+    }
+        
+    listitem.hide();
+    listitem.show("blind", 750);
+}
+
+var showError = function (text) {
+    if ( typeof(text) == "undefined" ) {
+        $("#error").hide("fold", 1000);
+    }
+    else {
+        $("#error").html(text);
+        $("#error").show("fold", 1000);
+    }
+}
+
+function getIsoDate(date) {
+    if ( typeof(date) == "undefined" ) {
+        date= new Date();
+    }
+
+    var year = date.getFullYear();
+    
+    var month = date.getMonth()+1;
+    if(month <= 9)
+        month = '0'+month;
+
+    var day= date.getDate();
+    if(day <= 9)
+        day = '0'+day;
+
+    var isoDate = year +'-'+ month +'-'+ day;
+    return isoDate;
+}
+
+function resetForm() {
+    $("#datepicker").val(getIsoDate());
+    $("#odometer").val("");
+    $("#amount").val("");
+    $("#price").val("");
+    showError();
+}
